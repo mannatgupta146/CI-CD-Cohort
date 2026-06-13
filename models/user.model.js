7f7fc293da35dbf78eb268df7fbe14a5c9b4d09c
Mannat Gupta <mannatgupta146@gmail.com>
2026-06-13 14:17:58 +0530

    pass for ci cd

--- models/user.model.js
+++ models/user.model.js
@@ -4,7 +4,7 @@ const userSchema = new mongoose.Schema({
     name: String,
     email: {
         type: String,
-       /*  required: true */
+        required: true
     }
 }, {timestamps: true});
 
